7862d24b4d29fb59c98b2c046a2f3cb6fe68cb01
RadySonabu <ardynubanos@yahoo.com>
2021-07-23 02:15:56 +0800

    Fixed AWS account

--- config/settings/local.py
+++ config/settings/local.py
@@ -31,29 +31,30 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 10,
 }
 CORS_ORIGIN_ALLOW_ALL = True
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 # AWS CONFIG
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'staticfiles-ardy'
-AWS_QUERYSTRING_AUTH = False
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-# AWS_S3_FILE_OVERWRITE = False
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_DEFAULT_ACL = 'public-read'
+
 AWS_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), "static"),)
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'config.settings.storages.MediaStore'
+
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# DEFAULT_FILE_STORAGE = 'config.settings.storages.MediaStore'
-
 # AWS_S3_VERIFY = True
 # AWS_S3_SIGNATURE_VERSION = 's3v4'
-# STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), "static"),)
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 # STATIC_URL = "/static/"
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # STATIC_ROOT = ""
